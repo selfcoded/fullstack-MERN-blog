3b497fb3d14c6098a1afb5b4d4f67ec94c00f9f9
selfcoded <haolovelyjiang@gmail.com>
2024-01-10 21:54:43 +0100

    setup css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>home
+    <div className="text-blue-600">home
     </div>
   )
 }
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Home } from '../pages/Home'
-import { Projects } from '../pages/Projects'
-import { About } from '../pages/About'
-import { Contact } from '../pages/Contact'
-import { Blog } from '../pages/Blog'
+import { Home } from './pages/Home'
+import { Projects } from './pages/Projects'
+import { About } from './pages/About'
+import { Contact } from './pages/Contact'
+import { Blog } from './pages/Blog'
+import { Login } from './pages/Login'
 import { createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom'
-import { ContentLayout } from '../layouts/ContentLayout'
+import { ContentLayout } from './layouts/ContentLayout'
 
 const router =  createBrowserRouter(createRoutesFromElements(
     <>
         <Route element={<ContentLayout />} >
             <Route index element={<Home />} />
+            <Route path='/admin/login' element={<Login />} />
             <Route path='/projects' element={<Projects />} />
             <Route path='/blog' element={<Blog />} />
             <Route path='/about' element={<About />} />
